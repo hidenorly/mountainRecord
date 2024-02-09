338f87d691eb735b6431395c7400cb34d1602b04
hidenorly <twitte.harold@gmail.com>
2024-02-10 04:10:12 +0900

    Add experimental actual recent record retrteiver

---
+++ get_recent_record2.py
@@ -0,0 +1,144 @@
+#   Copyright 2024 hidenorly
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import argparse
+import mountainDic
+import sys
+import subprocess
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+class MountainRecordUtil:
+	def __init__(self):
+		dic = mountainDic.getMountainDic()
+
+		mountainUrls = {}
+		for aMountain in dic:
+			if aMountain["url"]:
+				mountainUrls[ aMountain["url"] ] = aMountain
+
+			self.mountainDic = {}
+			for url, aMountain in mountainUrls.items():
+				if not aMountain["name"] in self.mountainDic:
+					self.mountainDic[ aMountain["name"] ] = []
+				self.mountainDic[ aMountain["name"] ].append( aMountain )
+
+	def getMountainsWithMountainNameFallback(self, mountainName):
+		result = []
+		for theMountainName, theMountains in self.mountainDic.items():
+			for theMountain in theMountains:
+				if theMountain["name"].find(mountainName)!=-1 or theMountain["yomi"].find(mountainName)!=-1:
+					result.append( theMountain )
+		return result
+
+
+	def getMountainsWithMountainName(self, mountainName):
+		result = []
+		if mountainName in self.mountainDic:
+			result = self.mountainDic[ mountainName ]
+		# fallback
+		if not result:
+			result = self.getMountainsWithMountainNameFallback(mountainName)
+		return result
+
+	def parseRecentRecord(self, url):
+		result = []
+		res = requests.get(url)
+		soup = BeautifulSoup(res.text, 'html.parser')
+		blocks = soup.select('#reclist .block')
+		for block in blocks:
+			date_elem = block.select_one('.ft')
+			date_text = 'N/A'
+			date_parsed = None
+			if date_elem:
+				date_text = date_elem.text.strip().split('（')[0]
+			try:
+				date_parsed = datetime.strptime(date_text, '%Y年%m月%d日').date()
+			except :
+				pass
+
+			level_tag = block.select('.spr1[class*=spr1-level]')
+			level = 'N/A'
+			if level_tag:
+				level_class = [tag['class'][1] for tag in level_tag]
+				level_mapping = {
+					'spr1-level_C': 'C',
+					'spr1-level_B': 'B',
+					'spr1-level_A': 'A',
+					'spr1-level_S': 'S',
+					'spr1-level_D': 'D'
+				}
+				for cls in level_class:
+					if cls in level_mapping:
+						level = level_mapping[cls]
+						break
+			photo = True if block.select_one('.spr1-ico_photo') else False
+			route = True if block.select_one('.spr1-ico_route') else False
+
+			url_elem = block.select_one('.title a')
+			if url_elem:
+				url = url_elem['href']
+			else:
+				url = 'N/A'
+
+			result.append({
+				'date_text': date_text,
+				'date': date_parsed,
+				'level': level,
+				'photo': photo,
+				'route': route,
+				'url': url
+			})
+		return result
+
+class ExecUtil:
+	@staticmethod
+	def _getOpen():
+		result = "open"
+		if sys.platform.startswith('win'):
+			result = "start"
+		return result
+
+	@staticmethod
+	def open(arg):
+		exec_cmd = f'{ExecUtil._getOpen()} {arg}'
+		result = subprocess.run(exec_cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+		return result
+
+
+if __name__=="__main__":
+	parser = argparse.ArgumentParser(description='Specify mountainNames', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('args', nargs='*', help='url encoded strings')
+	parser.add_argument('-nd', '--urlOnly', action='store_true', default=False, help='specify if you want to print url only')
+	parser.add_argument('-o', '--openUrl', action='store_true', default=False, help='specify if you want to open the url')
+	parser.add_argument('-f', '--filterLevel', action='store', default="D|C|B|A|S", help='specify if you want to filter the article info level')
+
+	args = parser.parse_args()
+	recUtil = MountainRecordUtil()
+	acceptableInfoLevel = args.filterLevel.split("|")
+
+	for aMountainName in args.args:
+		result = recUtil.getMountainsWithMountainName( aMountainName )
+		for aMountain in result:
+			results = recUtil.parseRecentRecord( aMountain["url"] )
+			for aResult in results:
+				if aResult["level"] in acceptableInfoLevel:
+					url = aResult["url"]
+					if args.urlOnly:
+						print( url )
+					else:
+						print( f'name:{aMountain["name"]}, yomi:{aMountain["yomi"]}, altitude:{aMountain["altitude"]} : {url}' )
+					if args.openUrl:
+						ExecUtil.open( url )
